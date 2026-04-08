b9753965777f10f88f22f136689ab61ec550905e
Gabriel Klaumann kraus <gabriel_kraus@estudante.sesisenai.org.br>
2026-04-08 14:55:36 -0300

    a

--- tecnotropolis/src/app/layout/footer/footer.ts
+++ tecnotropolis/src/app/layout/footer/footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
