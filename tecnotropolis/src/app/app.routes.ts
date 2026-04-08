9bb7eb1668397f870eefc68e6af49aa2ee315ed4
Gabriel Klaumann kraus <gabriel_kraus@estudante.sesisenai.org.br>
2026-04-08 14:59:02 -0300

    a

--- tecnotropolis/src/app/app.routes.ts
+++ tecnotropolis/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Curiosidades } from './pages/curiosidades/curiosidades';
+import { Home } from './pages/home/home';
+import { Situacao } from './pages/situacao/situacao';
+import { Solucao } from './pages/solucao/solucao';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home},
+    {path:'curiosidade',component:Curiosidades},
+    {path:'situacao',component:Situacao},
+    {path:'solucao',component:Solucao},
+];
